c27730bcac9773cf66c4cdc95a14a7faf41b84c0
Alexey Burak <byraklesha@gmail.com>
2023-07-03 17:15:11 +0300

    feat: add caching

--- src/main/java/com/burak/caching/Runner.java
+++ src/main/java/com/burak/caching/Runner.java
@@ -2,8 +2,10 @@ package com.burak.caching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Runner {
 
     public static void main(String[] args) {
